d697abd6e1f7506f90c4036dc3757ad36dcfa658
231 <snu231@github.com>
2021-01-24 19:55:57 +0900

    hooks validate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const useInput = (initialValue, validator) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    const{
+      target: {value}
+    } = event;
+    let willUpdate = true;
+
+    if( typeof validator ==="function"){
+      willUpdate = validator(value);
+    }
+
+    if(willUpdate){
+      setValue(value);
+    }
+
+  }
+  return {value, onChange} ;
+}
+
+const App = () => {
+
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
